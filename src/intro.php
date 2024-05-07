763de1987d460eb94cd3f813a6e4aad758078094
realpvblo <78243008+realpvblo@users.noreply.github.com>
2024-05-07 12:41:19 +0200

    set custom name for css and js build files

--- src/intro.php
+++ src/intro.php
@@ -2,18 +2,19 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="stylesheet" href="style.css" />
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-  <title>Strona Główna</title>
-  <script type="module" src="main.js" defer></script>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter+Tight:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <title>Strona Główna</title>
+    <script type="module" src="main.js" defer></script>
+    <link rel="stylesheet" href="/dist/prod.css">
 </head>
 
 <body>
-  <!-- <header class="header_wrapper">
+    <!-- <header class="header_wrapper">
     <a>
   	<svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="30" fill="#fff" height="30" viewBox="0 0 24 24">
 <path d="M 12 2.0996094 L 1 12 L 4 12 L 4 21 L 11 21 L 11 15 L 13 15 L 13 21 L 20 21 L 20 12 L 23 12 L 12 2.0996094 z M 12 4.7910156 L 18 10.191406 L 18 11 L 18 19 L 15 19 L 15 13 L 9 13 L 9 19 L 6 19 L 6 10.191406 L 12 4.7910156 z"></path>
@@ -36,78 +37,73 @@
         </div>
     </div>
   </div> -->
- 
- <nav class="mainNav">
- <div class="mainNav__logo"><a href="intro.php">MówiMY</a></div>
-    <div class="mainNav__links">
-       <a href="" class="mainNav__link">Zacznij naukę</a>
-       <a href="" class="mainNav__link">O nas</a>
-       <a href="" class="mainNav__link">Kontakt</a>
-       <a href="login.php" class="mainNav__link">Zaloguj się</a>
-    </div>
-    <div class="mainNav__icon">
-       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-          <g data-name="Layer 2" fill="#9197AE">
-             <g data-name="menu-2">
-                <rect
-                   width="24"
-                   height="24"
-                   transform="rotate(180 12 12)"
-                   opacity="0"
-                />
-                <circle cx="4" cy="12" r="1" />
-                <rect x="7" y="11" width="14" height="2" rx=".94" ry=".94" />
-                <rect x="3" y="16" width="18" height="2" rx=".94" ry=".94" />
-                <rect x="3" y="6" width="18" height="2" rx=".94" ry=".94" />
-             </g>
-          </g>
-       </svg>
-    </div>
- </nav>
- <header class="mainHeading">
-    <div class="mainHeading__content">
-        <article class="mainHeading__text">
-            <p class="mainHeading__preTitle">Mówimy</p>
-            <h2 class="mainHeading__title">Naucz się języków</h2>
-            <p class="mainHeading__description">
-                Zacznij naukę języków obcych z nami. Znajdziesz tutaj kursy dla każdego poziomu zaawansowania.
-            </p>
-            <button class="cta" onclick="redirectToRegister()">Załóż konto</button>
-        </article>
 
-        <figure class="mainHeading__image">
-            <img src="https://images.unsplash.com/photo-1535295972055-1c762f4483e5?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
-        </figure>
-    </div>
-</header>
+    <nav class="mainNav">
+        <div class="mainNav__logo"><a href="intro.php">MówiMY</a></div>
+        <div class="mainNav__links">
+            <a href="" class="mainNav__link">Zacznij naukę</a>
+            <a href="" class="mainNav__link">O nas</a>
+            <a href="" class="mainNav__link">Kontakt</a>
+            <a href="login.php" class="mainNav__link">Zaloguj się</a>
+        </div>
+        <div class="mainNav__icon">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <g data-name="Layer 2" fill="#9197AE">
+                    <g data-name="menu-2">
+                        <rect width="24" height="24" transform="rotate(180 12 12)" opacity="0" />
+                        <circle cx="4" cy="12" r="1" />
+                        <rect x="7" y="11" width="14" height="2" rx=".94" ry=".94" />
+                        <rect x="3" y="16" width="18" height="2" rx=".94" ry=".94" />
+                        <rect x="3" y="6" width="18" height="2" rx=".94" ry=".94" />
+                    </g>
+                </g>
+            </svg>
+        </div>
+    </nav>
+    <header class="mainHeading">
+        <div class="mainHeading__content">
+            <article class="mainHeading__text">
+                <p class="mainHeading__preTitle">Mówimy</p>
+                <h2 class="mainHeading__title">Naucz się języków</h2>
+                <p class="mainHeading__description">
+                    Zacznij naukę języków obcych z nami. Znajdziesz tutaj kursy dla każdego poziomu zaawansowania.
+                </p>
+                <button class="cta" onclick="redirectToRegister()">Załóż konto</button>
+            </article>
 
-<script>
-    function redirectToRegister() {
-        window.location.href = "register.php";
-    }
-</script>
+            <figure class="mainHeading__image">
+                <img src="https://images.unsplash.com/photo-1535295972055-1c762f4483e5?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
+            </figure>
+        </div>
+    </header>
 
- <div class="language_wrapper intro">
-    <a class="language" href="">
-        <img src="/images/germany-flag.png" >
-        <p>Niemiecki</p>
-    </a>
-    <a class="language">
-        <img src="/images/uk-flag.png">
-        <p>Angielski</p>
-    </a>
-    <div class="language">
-        <img src="/images/spain-flag.png">
-        <p>Hiszpański</p>
+    <script>
+        function redirectToRegister() {
+            window.location.href = "register.php";
+        }
+    </script>
+
+    <div class="language_wrapper intro">
+        <a class="language" href="">
+            <img src="/images/germany-flag.png">
+            <p>Niemiecki</p>
+        </a>
+        <a class="language">
+            <img src="/images/uk-flag.png">
+            <p>Angielski</p>
+        </a>
+        <div class="language">
+            <img src="/images/spain-flag.png">
+            <p>Hiszpański</p>
+        </div>
+        <div class="language">
+            <img src="/images/france-flag.jpg">
+            <p>Francuzki</p>
+        </div>
     </div>
-    <div class="language">
-        <img src="/images/france-flag.jpg">
-        <p>Francuzki</p>
-    </div>
-    </div>
- 
-  
-  <script src="script.js"></script>
+
+
+    <script src="script.js"></script>
 </body>
 
 </html>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,30 @@ import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src', // Aktualizacja ścieżki do głównego katalogu projektu
-  plugins: [],
+  plugins: [
+    {
+      name: 'custom-css-filename',
+      enforce: 'post',
+      generateBundle(options, bundle) {
+        for (const fileName in bundle) {
+          if (bundle[fileName].type === 'asset' && fileName.endsWith('.css')) {
+            bundle[fileName].fileName = 'prod.css'; // Tutaj możesz ustawić niestandardową nazwę pliku CSS
+          }
+        }
+      },
+    },
+    {
+      name: 'custom-js-filename',
+      enforce: 'post',
+      generateBundle(options, bundle) {
+        for (const fileName in bundle) {
+          if (bundle[fileName].type === 'chunk' && fileName.endsWith('.js')) {
+            bundle[fileName].fileName = 'prod.js'; // Tutaj możesz ustawić niestandardową nazwę pliku JS
+          }
+        }
+      },
+    },
+  ],
   css: {
     preprocessorOptions: {
       scss: {
